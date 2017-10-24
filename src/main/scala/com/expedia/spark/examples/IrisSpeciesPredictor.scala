a5edf2248d6d6bbf9ea6a9120186d6b40affae6d
phuriku <jaemberton@gmail.com>
2017-10-23 20:08:51 -0500

    Add Iris Dataset Cleaner.

---
+++ src/main/scala/com/expedia/spark/examples/IrisSpeciesPredictor.scala
@@ -0,0 +1,8 @@
+package com.expedia.spark.examples
+
+/**
+  * Created by jaemberton on 10/23/17.
+  */
+class IrisSpeciesPredictor {
+
+}
---
+++ src/main/scala/com/expedia/spark/util/IrisDatasetCleaner.scala
@@ -0,0 +1,33 @@
+package com.expedia.spark.util
+
+import java.io.FileWriter
+import java.nio.file.{Files, Paths}
+
+import scala.io.Source
+
+object IrisDatasetCleaner extends App {
+
+  val source = "iris_dataset.txt"
+  val target = "iris_dataset_cleaned.txt"
+
+  def extractSVMLib(): Unit = {
+    if (Files.exists(Paths.get(target))) Files.delete(Paths.get(target))
+    val fw = new FileWriter(target, true)
+    try {
+      Source.fromFile(source).getLines.foreach { line =>
+        fw.write(extractLine(line) + "\n")
+      }
+    } finally {
+      fw.close()
+    }
+  }
+
+  def extractLine(line: String): String = {
+    (line match {
+      case x: String if x.endsWith(",Iris-setosa") => s"1 1:${x.replaceAll(",Iris-setosa", "")}"
+      case x: String if x.endsWith(",Iris-versicolor") => s"2 1:${x.replaceAll(",Iris-versicolor", "")}"
+      case x: String if x.endsWith(",Iris-virginica") => s"3 1:${x.replaceAll(",Iris-virginica", "")}"
+    }).replaceFirst(",", " 2:").replaceFirst(",", " 3:").replaceFirst(",", " 4:")
+  }
+
+}
