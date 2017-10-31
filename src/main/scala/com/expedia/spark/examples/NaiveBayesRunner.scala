0af95482a6de5ab498131b7931cce473e90e452f
phuriku <jaemberton@gmail.com>
2017-10-31 00:20:47 -0500

    Naive Bayes.

--- src/main/scala/com/expedia/spark/examples/NaiveBayesRunner.scala
+++ src/main/scala/com/expedia/spark/examples/NaiveBayesRunner.scala
@@ -1,5 +1,35 @@
 package com.expedia.spark.examples
 
-class NaiveBayesRunner {
+import org.apache.spark.ml.classification.NaiveBayes
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.sql.SparkSession
 
+object NaiveBayesRunner extends App {
+
+  val spark = SparkSession
+    .builder()
+    .appName("Naive Bayes")
+    .master("local")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val trainingData = spark.read.format("libsvm")
+    .load("sample_gender_data.txt")
+
+  val nb = new NaiveBayes()
+    .setSmoothing(1.0)
+    .setModelType("multinomial")
+  val model = nb.fit(trainingData)
+
+  val testData = Seq((6, 130, 8))
+    .toDF("height", "weight", "foot size")
+  val testSet = new VectorAssembler()
+    .setInputCols(Array("height", "weight", "foot size"))
+    .setOutputCol("features")
+    .transform(testData)
+    .select("features")
+
+  val predictions =  model.transform(testSet)
+  predictions.show()
 }
